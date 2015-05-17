50b7769b43dc25f21a2aa5c776937ba90317975a
Marc MacLeod <Marc@Marcs-MacBook-Pro-2.local>
2015-05-17 15:14:12 -0500

    no more caching

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,9 +1,16 @@
 class Api::BaseController < ApplicationController
   # before_filter :authenticate_user!
+  before_filter :set_cache_buster
 
   private
 
   def permission_denied
     render json: {error: 'unauthorized'}, status: :unauthorized
+  end
+
+  def set_cache_buster
+    response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
   end
 end
